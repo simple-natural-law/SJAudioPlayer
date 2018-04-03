61d4e85cfc7475024654b56b0af765e7b38c6139
张诗健 <zsjcwy_xcode@163.com>
2018-04-03 16:11:31 +0800

    commit

---
+++ SJAudioStream/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  SJAudioStream
+//
+//  Created by 讯心科技 on 2018/4/3.
+//  Copyright © 2018年 讯心科技. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+//    NSString *urlString = @"http://hoishow-file.b0.upaiyun.com/uploads/boom_track/file/4ba30881757c35365380dbe9c9538054_ab320.mp3";
+//
+//    NSString *escapedValue =
+//    (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(
+//                                                                          nil,
+//                                                                          (CFStringRef)urlString,
+//                                                                          NULL,
+//                                                                          NULL,
+//                                                                          kCFStringEncodingUTF8));
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
