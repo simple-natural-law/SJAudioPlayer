ee79e11dc2530dc5e2c46744c6f6077f3f6e03da
张诗健 <zsjcwy_xcode@163.com>
2018-04-12 13:06:49 +0800

    commit

--- SJAudioStream/ViewController.m
+++ SJAudioStream/ViewController.m
@@ -24,7 +24,8 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
